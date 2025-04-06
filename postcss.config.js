d2a869267f8d27a102646b0b56dd5611812f7ed3
SunDevil311 <github@sl.neteng.cc>
2025-04-06 03:32:07 -0700

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+/*
+  SPDX-License-Identifier: CC-BY-4.0 OR GPL-3.0-or-later
+  This file is part of Network Pro
+*/
+
+module.exports = {
+  plugins: [require("autoprefixer")],
+};
